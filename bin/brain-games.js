b8aad0c9b59ca2b2449de91f59479119985b9e9c
Max <maximhop200307@gmail.com>
2024-10-04 17:22:44 +0300

    "Remove cli.js file and refactor lcm.js to use gameEngine.js"

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,13 @@
-import { greetUser } from '../src/cli.js';
-import { playLCMGame } from '../src/games/lcm.js';
+import readlineSync from 'readline-sync';
+import startLCMGame from '../src/games/lcm.js';
+import startProgressionGame from '../src/games/progression.js';
 
-greetUser();
-playLCMGame();
+const choice = readlineSync.question('Which game would you like to play? Choose "lcm" or "progression": ');
+
+if (choice === 'lcm') {
+  startLCMGame();
+} else if (choice === 'progression') {
+  startProgressionGame();
+} else {
+  console.log('Invalid choice! Please choose either "lcm" or "progression".');
+}
